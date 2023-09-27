0f8dc7f66fba8827bdf4d8bdcdc87bd4b42c73c8
fardin-developer <fardindeveloper1@gmail.com>
2023-09-27 10:59:45 +0530

    umananda branch

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Content from "@/components/contents/Content";
+import Event from "@/components/events/Event"
+
+export default function Home() {
+  return (
+    <main>
+      <Content />
+      <Event />
+    </main>
+  );
+}
